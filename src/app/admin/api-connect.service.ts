49d4ac554bd25e91c15b42d4a0e4ed64bf43677c
channuwat <channuwat101@gmail.com>
2023-04-19 11:25:35 +0700

    setting api

---
+++ src/app/admin/api-connect.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiConnectService {
+
+  constructor(public http: HttpClient) { }
+
+  getData(method: string) {
+    return new Promise((reject, resove) => {
+      this.http.get(environment.host + method).subscribe((res: any) => {
+        reject(res)
+      }, (error) => {
+        resove(error)
+      })
+    })
+  }
+
+  postData(method: string, data: any) {
+    return new Promise((reject, resove) => {
+      this.http.post(environment.host + method, data).subscribe((res: any) => {
+        reject(res)
+      }, (error) => {
+        resove(error)
+      })
+    })
+  }
+
+  getStore(key: string) {
+    const store: any = localStorage.getItem(key)
+    return JSON.parse(store)
+  }
+
+  setStore(key: string, val: any) {
+    localStorage.setItem(key, JSON.stringify(val))
+  }
+
+  // save_success() {
+  //   Swal.fire({
+  //     position: 'top-end',
+  //     icon: 'success',
+  //     title: 'บันทึกสำเร็จ...',
+  //     showConfirmButton: false,
+  //     timer: 1500
+  //   })
+  // }
+
+  // save_error() {
+  //   Swal.fire({
+  //     position: 'top-end',
+  //     icon: 'error',
+  //     title: 'บันทึกผิดผลาาด!',
+  //     showConfirmButton: false,
+  //     timer: 1500
+  //   })
+  // }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  host : 'http://192.168.1.149/api/index.php/',
+  path : 'http://192.168.1.149/api/'
 };
+
